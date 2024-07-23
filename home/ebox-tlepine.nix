ce90bc7cd8a7e6969ac99e526847538ab18575c1
Tristan Lepine <tristan.lepine@ebox.ca>
2024-07-23 17:02:11 -0400

    more vim configuration

--- home/ebox-tlepine.nix
+++ home/ebox-tlepine.nix
@@ -73,9 +73,16 @@ in
     enable = true;
     plugins = with pkgs.vimPlugins; [
       vim-markdown
+
+      # colorscheme
+      papercolor-theme
     ];
     extraConfig = ''
+      colorscheme PaperColor
+      set background=dark
       set conceallevel=2
+
+      autocmd FileType markdown set textwidth=50
     '';
   };
 }
