57ebfe3d3a2b5cdc5559015f6677ea369985a49b
Tristan Lepine <tristanlepine14@gmail.com>
2024-09-09 20:34:30 -0400

    changed vim configuration

--- home/modules/vim.nix
+++ home/modules/vim.nix
@@ -1,12 +1,30 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
+let
+  # TODO: move custom vim plugins to their own file (or git repo)
+  extraPlugins = {
+    tokyonight-vim = pkgs.vimUtils.buildVimPlugin {
+      name = "tokyonight.vim";
+      version = "1.0";
+      src = pkgs.fetchFromGitHub {
+        owner = "ghifarit53";
+        repo = "tokyonight-vim";
+        rev = "4e82e0f0452a6ce8f387828ec71013015515035a";
+        sha256 = "sha256-ui/6xv8PH6KuQ4hG1FNMf6EUdF2wfWPNgG/GMXYvn/8=";
+      };
+    };
+  };
+in
 {
   programs.vim = {
     enable = true;
     plugins = with pkgs.vimPlugins; [
       vim-markdown
+      extraPlugins.tokyonight-vim
     ];
     extraConfig = ''
-      colorscheme habamax
+      set termguicolors
+
+      colorscheme tokyonight
       set background=dark
       set conceallevel=2
 
