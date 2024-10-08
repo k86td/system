c31e4ebb6ef676c882767f5150e1054ce825768a
Tristan Lepine <tristan.lepine@ebox.ca>
2024-10-08 16:30:27 -0400

    added starship.toml configuration

--- home/ebox-tlepine.nix
+++ home/ebox-tlepine.nix
@@ -61,6 +61,14 @@ rec {
         source = ./files/nix;
         recursive = true;
     };
+    "${homeDirectory}/.config/starship.toml" = {
+        source = ./files/starship.toml;
+    };
+  };
+
+  programs.starship = {
+    enable = true;
+    enableNushellIntegration = true;
   };
 
   home.sessionVariables = {
@@ -77,6 +85,26 @@ rec {
       sw = "home-manager --flake /etc/nixos#ebox-tlepine switch";
       dev = "nix develop -c $env.SHELL";
     };
+    extraConfig = ''
+      $env.STARSHIP_SHELL = "nu"
+
+      def create_left_prompt [] {
+          starship prompt --cmd-duration $env.CMD_DURATION_MS $'--status=($env.LAST_EXIT_CODE)'
+      }
+
+      # Use nushell functions to define your right and left prompt
+      $env.PROMPT_COMMAND = { || create_left_prompt }
+      $env.PROMPT_COMMAND_RIGHT = ""
+
+      # The prompt indicators are environmental variables that represent
+      # the state of the prompt
+      $env.PROMPT_INDICATOR = ""
+      $env.PROMPT_INDICATOR_VI_INSERT = ": "
+      $env.PROMPT_INDICATOR_VI_NORMAL = "〉"
+      $env.PROMPT_MULTILINE_INDICATOR = "::: "
+
+      $env.PATH = '/home/tlepine/.nix-profile/bin'
+    '';
   };
 
   # Let Home Manager install and manage itself.
