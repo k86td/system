907ada951fd593be10b6a02772efe6e28578f4f3
Tristan Lepine <tristanlepine14@gmail.com>
2026-02-08 11:41:22 -0500

    fix: added caches modules first to see if it helps

--- modules/hosts/lenoovo-pad.nix
+++ modules/hosts/lenoovo-pad.nix
@@ -2,10 +2,10 @@
   flake.nixosConfigurations.lenoovo-pad = inputs.nixpkgs.lib.nixosSystem {
     system = "x86_64-linux";
     modules = [
+      inputs.self.nixosModules.caches
       ../../hosts/lenoovo-pad/configuration.nix
       inputs.determinate.nixosModules.default
       inputs.self.nixosModules.openclaw
-      inputs.self.nixosModules.caches
     ];
   };
 }
