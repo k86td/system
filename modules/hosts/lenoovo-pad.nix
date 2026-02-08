6658eaa2d9e1e3a988b84f002905cf3b39e8b49f
Tristan Lepine <tristanlepine14@gmail.com>
2026-02-08 12:32:06 -0500

    fix: hm and nixos-rebuild

--- modules/hosts/lenoovo-pad.nix
+++ modules/hosts/lenoovo-pad.nix
@@ -2,10 +2,22 @@
   flake.nixosConfigurations.lenoovo-pad = inputs.nixpkgs.lib.nixosSystem {
     system = "x86_64-linux";
     modules = [
-      inputs.self.nixosModules.caches
       ../../hosts/lenoovo-pad/configuration.nix
       inputs.determinate.nixosModules.default
       inputs.self.nixosModules.openclaw
+      inputs.self.nixosModules.caches
+
+      inputs.home-manager.nixosModules.home-manager
+      {
+        home-manager.useGlobalPkgs = true;
+        home-manager.useUserPackages = true;
+        home-manager.users.tlepine = {
+          imports = [
+            ../../home/new-tlepine.nix
+            inputs.zen-browser.homeModules.beta
+          ];
+        };
+      }
     ];
   };
 }
