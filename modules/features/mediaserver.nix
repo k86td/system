3aae6328200a2c36527af17f5008ad2030b5ca6e
Tristan Lepine <tristanlepine14@gmail.com>
2026-07-27 20:30:24 -0400

    feat: publish mediaserver services

--- modules/features/mediaserver.nix
+++ modules/features/mediaserver.nix
@@ -22,6 +22,63 @@
       };
 
       environment.systemPackages = with pkgs; [ vlc ];
+
+      services.avahi = {
+        enable = true;
+        nssmdns4 = true;
+        openFirewall = true;
+        publish = {
+          enable = true;
+          addresses = true;
+          userServices = true;
+        };
+        extraServiceFiles = {
+          jellyfin = pkgs.writeText "jellyfin.service" ''
+            <?xml version="1.0" standalone='no'?>
+            <!DOCTYPE service-group SYSTEM "avahi-service.dtd">
+            <service-group>
+              <name replace-wildcards="yes">Jellyfin on %h</name>
+              <service>
+                <type>_http._tcp</type>
+                <port>8096</port>
+              </service>
+            </service-group>
+          '';
+          radarr = pkgs.writeText "radarr.service" ''
+            <?xml version="1.0" standalone='no'?>
+            <!DOCTYPE service-group SYSTEM "avahi-service.dtd">
+            <service-group>
+              <name replace-wildcards="yes">Radarr on %h</name>
+              <service>
+                <type>_http._tcp</type>
+                <port>7878</port>
+              </service>
+            </service-group>
+          '';
+          prowlarr = pkgs.writeText "prowlarr.service" ''
+            <?xml version="1.0" standalone='no'?>
+            <!DOCTYPE service-group SYSTEM "avahi-service.dtd">
+            <service-group>
+              <name replace-wildcards="yes">Prowlarr on %h</name>
+              <service>
+                <type>_http._tcp</type>
+                <port>9696</port>
+              </service>
+            </service-group>
+          '';
+          qbittorrent = pkgs.writeText "qbittorrent.service" ''
+            <?xml version="1.0" standalone='no'?>
+            <!DOCTYPE service-group SYSTEM "avahi-service.dtd">
+            <service-group>
+              <name replace-wildcards="yes">qBittorrent on %h</name>
+              <service>
+                <type>_http._tcp</type>
+                <port>8080</port>
+              </service>
+            </service-group>
+          '';
+        };
+      };
     };
   };
 }
